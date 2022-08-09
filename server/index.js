809bee417c0fe6dd28d3565e989018dbbc97b023
B-jisoo <mrk334kr@naver.com>
2022-08-10 08:55:58 +0900

    정보 보호

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-
 // 몽고 DB 연결, 실패시 에러 출력
 mongoose
-  .connect(
-    "mongodb+srv://jisoo:jisoo@boilerplate.jfgm4sm.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connectd..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
