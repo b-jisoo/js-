76b5397543284da4c04f85bb5713fc661c68c13e
B-jisoo <mrk334kr@naver.com>
2022-08-26 06:41:58 +0900

    장바구니 마무리

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app";
-import store from "./redux";
-import { getTotals } from "./redux/cartSlice";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
--- client/src/pages/cart/index.tsx
+++ client/src/pages/cart/index.tsx
@@ -1,21 +1,27 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux";
-import { clearCart } from "../../redux/cartSlice";
+import { clearCart, getTotals } from "../../redux/cartSlice";
 import { CartPage, Product } from "../../type";
 import CartItem from "./items";
 
 const Cart = () => {
   const cartData = useSelector((state: RootState) => state.cart);
+  //const { cartTotalQuantity } = useSelector((state: RootState) => state.cart);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getTotals(cartData));
+  }, [cartData]);
+
   const handleClearCart = () => {
-    dispatch(clearCart(null));
+    dispatch(clearCart(cartData));
   };
 
   return (
     <div>
-      장바구니 페이지입니다
+      장바구니 페이지입니다 {cartData.cartTotalQuantity}개의 상품이 담겼습니다.
       {cartData.cartItems.length === 0 ? (
         <div>
           <p>담긴 상품이 없습니다.</p>
@@ -29,7 +35,7 @@ const Cart = () => {
             <button onClick={() => handleClearCart()}>Clear Cart</button>
             <div>
               <span>subtotal</span>
-              <span>${cartData.cartTotalAmount}</span>
+              <span>${cartData.cartTotalAmount.toFixed(2)}</span>
             </div>
           </div>
         </div>
